a4e8c2edbd7af0eaf91861a3cd52ec65fb9ea221
Ante Perisic <perisic.ante@gmail.com>
2025-03-13 22:49:30 +0100

    change ip to 0000

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let app = create_app(pool);
 
     //let addr = SocketAddr::from(([127, 0, 0, 1], config.port));
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
 
     let listener = TcpListener::bind(addr).await?;
 
